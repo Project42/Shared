8e512f3235059bab5f03191440eb23d0ec4b4878
Radek Slupik <daknok@icloud.com>
2013-01-22 18:27:14 +0100

    Add GameOverWorld and HighScoreWorld

---
+++ GameOverWorld.java
@@ -0,0 +1,22 @@
+import greenfoot.*;
+
+public class GameOverWorld extends World {
+    private int actsPassed;
+    private Game game;
+    
+    public GameOverWorld() {
+        super(600, 400, 1);
+    }
+    
+    public GameOverWorld(Game game) {
+        super(600, 400, 1); 
+        this.game = game;
+    }
+    
+    @Override
+    public void act() {
+        if (++actsPassed == 20) {
+            Greenfoot.setWorld(new HighScoreWorld(game));
+        }
+    }
+}
---
+++ HighScoreWorld.java
@@ -0,0 +1,10 @@
+import greenfoot.*;
+
+public class HighScoreWorld extends World {
+    private Game game;
+    
+    public HighScoreWorld(Game game) {
+        super(600, 400, 1);
+        this.game = game;
+    }
+}
